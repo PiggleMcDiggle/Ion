8f8430db83bfcf71129ccbf2f62a8b4f6a1ba22b
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-12-20 21:10:38 -0700

    Revert "Revert "Beginnings of a new system for custom items""
    This reverts commit ddb5c255ab1aae0bd1dc7028f02c47f1e2657b17.

---
+++ StarLegacy/src/main/kotlin/net/starlegacy/feature/misc/customitems/CustomItemManager.kt
@@ -0,0 +1,52 @@
+package net.starlegacy.feature.misc.customitems
+
+import net.horizonsend.ion.Ion.Companion.plugin
+import org.bukkit.NamespacedKey
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.persistence.PersistentDataType
+
+class CustomItemManager: Listener {
+	companion object {
+		val customItems = mutableMapOf<String, CustomItem>()
+	}
+
+	init {
+		plugin.server.pluginManager.registerEvents(this, plugin)
+	}
+
+	fun register(item: CustomItem) {
+		// Check for duplicate custom model data
+		customItems.forEach{ (id, customItem) ->
+			if (customItem.model == item.model && customItem.material == item.material) {
+				plugin.logger.warning("Multiple custom items have registered for the same material and model data!")
+				plugin.logger.warning("${customItem.id} and ${item.id} are both using ${customItem.material.name} and ${customItem.model}")
+			}
+		}
+		if (customItems.put(item.id, item) != null) {
+			plugin.logger.warning("Multiple custom items with id ${item.id} have been registered!")
+		}
+	}
+
+	@EventHandler
+	fun onInteract(event: PlayerInteractEvent) {
+		if (event.item == null) return
+		val item = customItems[
+				event.item!!.itemMeta.persistentDataContainer.get(
+					NamespacedKey(plugin, "custom_item_id"), PersistentDataType.STRING
+				) ?: return]
+			?: return
+
+		when (event.action) {
+			Action.LEFT_CLICK_BLOCK, Action.LEFT_CLICK_AIR -> {
+				 item.onLeftClick(event)
+			}
+			Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> {
+				item.onRightClick(event)
+			}
+			else -> return // ugh
+		}
+	}
+}
