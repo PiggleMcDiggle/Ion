20d399ae2799ad1ce0a18a9644205d0eed64cc42
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-12-26 20:27:05 -0700

    Move gear.kt's contents to elsewhere

--- StarLegacy/src/main/kotlin/net/starlegacy/feature/customitem/EnergySwords.kt
+++ StarLegacy/src/main/kotlin/net/starlegacy/feature/customitem/EnergySwords.kt
@@ -1,6 +1,10 @@
 package net.starlegacy.feature.customitem
 
+import net.horizonsend.ion.Ion.Companion.plugin
 import net.starlegacy.feature.customitem.type.EnergySwordItem
+import net.starlegacy.feature.gear.Gear
+import net.starlegacy.util.Tasks
+import org.bukkit.Bukkit
 import org.bukkit.Material
 
 object EnergySwords {
@@ -23,6 +27,34 @@ object EnergySwords {
 		registerEnergySword("green", 4)
 		registerEnergySword("purple", 5)
 		registerEnergySword("orange", 6)
-	}
 
+		// Energy sword idle sound
+		// Use async task and while loop with thread sleep so when it lags it doesnt sound weird
+		// The timing of the sounds is very important
+		Tasks.async {
+			while (plugin.isEnabled) {
+				Tasks.sync {
+					for (player in Bukkit.getOnlinePlayers()) {
+						val main = player.inventory.itemInMainHand
+						val offhand = player.inventory.itemInOffHand
+
+						val mainCustomItem = CustomItems[main]
+						val offhandCustomItem = CustomItems[offhand]
+
+						if (mainCustomItem != null && mainCustomItem.id.contains("sword")
+							|| offhandCustomItem != null && offhandCustomItem.id.contains("sword")
+						) {
+							player.world.playSound(player.location, "energy_sword.idle", 5.0f, 1.0f)
+						}
+					}
+				}
+
+				try {
+					Thread.sleep(2000)
+				} catch (e: InterruptedException) {
+					e.printStackTrace()
+				}
+			}
+		}
+	}
 }
