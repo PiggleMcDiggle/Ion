beeb24edd678e46fea707b94c0ec4252861b4c14
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-12-28 17:30:45 -0700

    Battery recipes

--- StarLegacy/src/main/kotlin/net/starlegacy/feature/customitem/BatteryItems.kt
+++ StarLegacy/src/main/kotlin/net/starlegacy/feature/customitem/BatteryItems.kt
@@ -1,13 +1,16 @@
 package net.starlegacy.feature.customitem
 
+import net.starlegacy.feature.customitem.CustomItems.Companion.recipeChoice
+import net.starlegacy.feature.customitem.CustomItems.Companion.registerShapedRecipe
 import net.starlegacy.feature.customitem.type.BatteryItem
 import net.starlegacy.util.SLTextStyle
+import net.starlegacy.util.Tasks
 import net.starlegacy.util.stripColor
 import org.bukkit.Material
 
 object BatteryItems {
 
-	private fun registerBattery(name: String, model: Int, power: Int): BatteryItem {
+	private fun registerBattery(name: String, model: Int, power: Int, craft: Material): BatteryItem {
 		val item = BatteryItem(
 			id = name.stripColor().lowercase().replace(" ", "_"),
 			displayName = name,
@@ -16,14 +19,19 @@ object BatteryItems {
 			maxPower = power,
 		)
 		CustomItems.register(item)
+		// Delay one tick so other items can register first, before trying to use them for crafting
+		Tasks.syncDelay(1){
+			registerShapedRecipe(item.id, item.getItem(), "aba", "aba", "aba", ingredients = mapOf(
+				'a' to recipeChoice(CustomItems["aluminum"]!!),
+				'b' to recipeChoice(craft)
+			))
+		}
 		return item
 	}
 
 	fun register() {
-		registerBattery("Battery ${SLTextStyle.RED}A", 7, 500)
-		registerBattery("Battery ${SLTextStyle.GREEN}M", 8, 1000)
-		registerBattery("Battery ${SLTextStyle.GOLD}G", 9, 2000)
+		registerBattery("Battery ${SLTextStyle.RED}A", 7, 500, Material.GLOWSTONE_DUST)
+		registerBattery("Battery ${SLTextStyle.GREEN}M", 8, 1000, Material.REDSTONE)
+		registerBattery("Battery ${SLTextStyle.GOLD}G", 9, 2000, Material.SEA_LANTERN)
 	}
-
 }
-
