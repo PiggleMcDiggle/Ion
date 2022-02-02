be492bd3352baaf17fd1d8d58fb2d5ff8b5050fa
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-02-02 16:32:36 -0700

    Use MiniMessage for gas canister colors

--- server/Ion/src/main/kotlin/net/horizonsend/ion/server/customitems/GasItems.kt
+++ server/Ion/src/main/kotlin/net/horizonsend/ion/server/customitems/GasItems.kt
@@ -3,20 +3,13 @@ package net.horizonsend.ion.server.customitems
 import net.horizonsend.ion.server.customitems.CustomItems.Companion.registerShapedRecipe
 import net.horizonsend.ion.server.customitems.types.GasItem
 import net.starlegacy.util.Tasks
-import net.starlegacy.util.stripColor
-import org.bukkit.ChatColor.AQUA
-import org.bukkit.ChatColor.DARK_PURPLE
-import org.bukkit.ChatColor.GREEN
-import org.bukkit.ChatColor.RED
-import org.bukkit.ChatColor.WHITE
-import org.bukkit.ChatColor.YELLOW
 import org.bukkit.Material.SNOWBALL
 
 object GasItems {
 
-	private fun registerGas(name: String, model: Int): GasItem {
+	private fun registerGas(id: String, name: String, model: Int): GasItem {
 		val item = GasItem(
-			id = "gas_canister_${name.stripColor().lowercase().replace(" ", "_")}",
+			id = "gas_canister_${id}",
 			displayName = name,
 			material = SNOWBALL,
 			model = model
@@ -26,7 +19,7 @@ object GasItems {
 	}
 
 	fun register() {
-		val empty = registerGas("${WHITE}Empty", model = 1)
+		val empty = registerGas("empty", "<white>Empty", model = 1)
 		Tasks.syncDelay(1) {
 			registerShapedRecipe(
 				empty.getItem(),
@@ -37,10 +30,10 @@ object GasItems {
 				)
 			)
 		}
-		registerGas("${YELLOW}Helium", model = 2)
-		registerGas("${AQUA}Oxygen", model = 3)
-		registerGas("${GREEN}Hydrogen", model = 4)
-		registerGas("${DARK_PURPLE}Nitrogen", model = 5)
-		registerGas("${RED}Carbon Dioxide", model = 6)
+		registerGas("helium","<yellow>Helium", model = 2)
+		registerGas("oxygen", "<aqua>Oxygen", model = 3)
+		registerGas("hydrogen","<green>Hydrogen", model = 4)
+		registerGas("nitrogen", "<dark_purple>Nitrogen", model = 5)
+		registerGas("carbon_dioxide","<red>Carbon Dioxide", model = 6)
 	}
 }
